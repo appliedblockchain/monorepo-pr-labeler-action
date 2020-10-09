1cd45fc49e9bac6528bc305f1740ff68d1142248
Ricardo Pinheiro <ricardo.rocha.pinheiro@gmail.com>
2020-10-09 15:08:53 +0100

    update prefix

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ async function prMonorepoRepoLabeler() {
     if (repo) {
       console.log(`labeling repo: ${repo}`)
 
-      const repoLabel = `📁 Repo: ${repo}`
+      const repoLabel = `Repo - ${repo}`
 
       helpers.addLabel(
         octokit,
